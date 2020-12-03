5b4d1e975bf2358b442893fdba8873ad03b01c31
Arie van der Poel <arie.theodorus.vanderpoel@gmail.com>
2020-12-03 18:07:51 +1300

    Added map component

--- config/dotenv.js
+++ config/dotenv.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 module.exports = function(/* env */) {
   return {
-    clientAllowedKeys: [],
+    clientAllowedKeys: ['MAPBOX_ACCESS_TOKEN'],
     fastbootAllowedKeys: [],
     failOnMissingKey: false,
     path: path.join(path.dirname(__dirname), '.env')
